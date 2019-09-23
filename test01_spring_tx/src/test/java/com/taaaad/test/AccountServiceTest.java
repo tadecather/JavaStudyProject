31e5937d9b2029d575978aeb51fd8368c99a70b8
tadecather <tadecather@gmail.com>
2019-09-23 20:49:01 +0800

    spring 声明式事务配置基础设置

---
+++ test01_spring_tx/src/test/java/com/taaaad/test/AccountServiceTest.java
@@ -0,0 +1,37 @@
+package com.taaaad.test;
+
+
+import com.taaaaad.domain.Account;
+import com.taaaaad.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import java.util.List;
+
+/**
+ * 使用Junit 进行单元测试
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService as;
+
+
+    /**
+     * 测试转账功能
+     */
+    @Test
+    public void testTransfer(){
+        as.transfer("aaa", "bbb", 100f);
+    }
+
+
+
+}
