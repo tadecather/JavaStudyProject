ba5217155f4041cf96ab273666cb67614538cf6a
tadecather <tadecather@gmail.com>
2019-10-17 08:44:36 +0800

    springmvc 入门程序创建

---
+++ SpringMVC/SpringMVC_01_start/src/main/java/com/taaaaad/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.taaaaad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Desc 控制器的类
+ * @Author taaaaad
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("Hello SpringMVC");
+        return "success";
+    }
+}
---
+++ SpringMVC/SpringMVC_01_start/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liubin23956
+  Date: 2019-10-16
+  Time: 9:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h3>入门程序</h3>
+    <a href="hello">入门程序</a>
+</body>
+</html>
